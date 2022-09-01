c3ef4622b93a269750e774efb6262b95ac74048f
Franco Fleitas <francofleitasguti@gmail.com>
2022-09-01 16:59:50 -0300

    añadiendo setTimeout y promise

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Item from './Item';
+
 
 import ItemList from './ItemList';
 
@@ -7,22 +7,27 @@ import ItemList from './ItemList';
 const ItemListContainer = () => {
     const [items, setItems] = useState([]);
 
-    const fetchItems = async () => {
-        const response = await fetch(
-            "https://api.jsonbin.io/v3/b/6310e8f35c146d63ca89a884",
-            {
-                headers: {
-                    "X-Master-Key":
-                        "$2b$10$NIzH8KUS7.zPD94h96DpIevwUPLU6IyfJ.FBJlfIR1zHJAUbZF/hW",
-                    "X-BIN-META": false
-                },
-            }
-        );
-        const data = await response.json();
-        return data;
-    }
+
     useEffect(() => {
-        fetchItems().then((data) => setItems(data));
+        const fetchItems = async () => {
+            const response = await fetch(
+                "https://api.jsonbin.io/v3/b/6310e8f35c146d63ca89a884",
+                {
+                    headers: {
+                        "X-Master-Key":
+                            "$2b$10$NIzH8KUS7.zPD94h96DpIevwUPLU6IyfJ.FBJlfIR1zHJAUbZF/hW",
+                        "X-BIN-META": false
+                    },
+                }
+            );
+            const sleep = (ms) =>{
+                return new Promise((resolve) => setTimeout(resolve, ms));
+            }
+            await sleep(2000)
+            const data = await response.json();
+            setItems(data)
+        }
+        fetchItems()
     }, []);
 
     return(
